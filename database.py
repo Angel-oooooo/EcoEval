6a125b95f8b63472fc4b8779a78acb0528d632d2
Rogelio Castillo <Rogelioy20031@hotmail.com>
2026-06-09 15:12:17 -0600

    update: listo para produccion

--- database.py
+++ database.py
@@ -1,19 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-# Aquí se crea el archivo de base de datos SQLite
-SQLALCHEMY_DATABASE_URL = "sqlite:///./ecoeval.db"
+load_dotenv()
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./ecoeval.db")
+
+connect_args = {"check_same_thread": False} if SQLALCHEMY_DATABASE_URL.startswith("sqlite") else {}
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=connect_args)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-# Esta función la usamos en cada endpoint para abrir y cerrar la BD
 def get_db():
     db = SessionLocal()
     try:
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from database import engine, Base
+from dotenv import load_dotenv
 import models
 import routers.auth_router as auth_router
 import routers.cursos_router as cursos_router
@@ -8,14 +9,19 @@ import routers.examenes_router as examenes_router
 import routers.dashboard_router as dashboard_router
 import routers.reportes_router as reportes_router
 import routers.usuarios_router as usuarios_router
+import os
+
+load_dotenv()
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="EcoEval - Sistema de Capacitación ISO 14001")
 
+origins = os.getenv("ALLOWED_ORIGINS", "http://localhost:5173,http://localhost:3000").split(",")
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
